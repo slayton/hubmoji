cfecb1e06997a9d5fb935a478580bc612f741190
Stuart Layton <stuart.layton@sevenrooms.com>
2023-10-03 15:56:44 -0400

    Commit of tools for pulling down emoji

---
+++ get_emoji.py
@@ -0,0 +1,41 @@
+import argparse
+import csv
+import urllib.request
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('name', type=str)
+parser.add_argument('-d', action='store_true')
+
+
+def load():
+    with open('emoji-urls.csv', mode='r') as infile:
+        reader = csv.reader(infile)
+        return dict((rows[0], rows[1]) for rows in reader)
+
+
+def download(name, url):
+    ext = url.rsplit(".", 1)[1]
+    dest_file = "results/" + name + "." + ext
+    if os.path.isfile(dest_file):
+        return
+    print ('downloading...\t' + dest_file)
+    urllib.request.urlretrieve(url, "results/" + name + "." + ext)
+
+
+def main():
+    args = parser.parse_args()
+    print(args.d)
+    print("looking for: [" + args.name + "]")
+    idx = 0
+    emojis = load()
+    keys = emojis.keys()
+    for key in keys:
+        if args.name in key:
+            print(key + "\n-------------\n" + emojis[key]+'\n')
+            if args.d:
+                download(key, emojis[key])
+
+
+if __name__=="__main__":
+    main()
