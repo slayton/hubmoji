ca2a8d7d16b45e4a96092ca3c1494909526f1950
Stuart Layton <stuart.layton@sevenrooms.com>
2025-08-05 10:08:47 -0400

    add code to create results dir if not found

--- get_emoji.py
+++ get_emoji.py
@@ -23,13 +23,20 @@ def download(name, url):
     urllib.request.urlretrieve(url, "results/" + name + "." + ext)
 
 
+def ensure_results_dir():
+    if not os.path.exists("results"):
+        os.makedirs("results")
+
 def main():
+
     args = parser.parse_args()
     print(args.d)
     print("looking for: [" + args.name + "]")
     idx = 0
     emojis = load()
     keys = emojis.keys()
+    if args.d:
+        ensure_results_dir()
     for key in keys:
         if args.name in key:
             print(key + "\n-------------\n" + emojis[key]+'\n')
